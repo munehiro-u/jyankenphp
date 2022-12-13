57a11215ebcf23f8cad59e67fb5346eab817c70e
munehiro-u <u0630@m-stillalive.com>
2022-12-13 20:57:22 +0900

    DB

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <?php
-$hands=['ぐー','ちょき','ぱー'];
+// DBへ接続
+try{
+    $dbh = new PDO($dsn, $user, $password);
+    // クエリの実行
+    $query = "SELECT * FROM jyannkenn";
+    $stmt = $dbh->query($query);
+
+    // 表示処理
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $hands[] = $row['name'];
+    }
+}catch(PDOException $e){
+    print("データベースの接続に失敗しました".$e->getMessage());
+    die();
+}
+
+// 接続を閉じる
+$dbh = null;
+
 $picts=['gu','choki','pa'];
 $results=['あいこ','アナタのまけです...','アナタのかちです！'];
 if(isset($_POST['hand'])){
-  $userHand=(int)$_POST['hand'];
-  $pcHand=rand(0,count($hands)-1);
+    $userHand=(int)$_POST['hand'];
+//  $pcHand=rand(0,count($hands)-1);
+    $pcHand = ($userHand + 2) % 3;
 }
- ?>
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,17 +38,17 @@ if(isset($_POST['hand'])){
 <body>
 <form method="post">
 <?php for($i=0;$i<count($hands);$i++):?>
-  <?php $checked=isset($userHand) && $userHand===$i ? 'checked':'';?>
-  <input type="radio" name="hand" value="<?=$i?>" <?=$checked?>><?=$hands[$i]?><br>
+<?php $checked=isset($userHand) && $userHand===$i ? 'checked':'';?>
+<input type="radio" name="hand" value="<?=$i?>" <?=$checked?>><?=$hands[$i]?><br>
 <?php endfor;?>
-  <button type="submit">ショウブ</button>
+<button type="submit">ショウブ</button>
 </form>
 <?php if(isset($_POST['hand'])):?>
-  <div>
-    <img src="images/<?=$picts[$userHand]?>.png">
-    <img src="images/<?=$picts[$pcHand]?>.png">
-  </div>
-  <p><?=$results[($userHand + 3 -$pcHand) % 3]?></p>
+<div>
+<img src="images/<?=$picts[$userHand]?>.png">
+<img src="images/<?=$picts[$pcHand]?>.png">
+</div>
+<p><?=$results[($userHand + 3 -$pcHand) % 3]?></p>
 <?php endif;?>
 </body>
 </html>
